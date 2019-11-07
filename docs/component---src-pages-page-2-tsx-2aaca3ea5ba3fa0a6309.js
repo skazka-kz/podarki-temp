b600a1e058470e3f477afa5c77178886a23ae2a9
Slava Bezgachev <bezgachev@gmail.com>
2019-11-07 10:06:41 +0600

    Added Docs

---
+++ docs/component---src-pages-page-2-tsx-2aaca3ea5ba3fa0a6309.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{212:function(e,t,a){"use strict";a.r(t);var n=a(77),o=a(0),l=a.n(o),c=a(210),r=a(209);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-2aaca3ea5ba3fa0a6309.js.map
