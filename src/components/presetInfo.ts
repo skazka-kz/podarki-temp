e929cd70484e73f16f5b610e04582299bd1da684
Slava Bezgachev <bezgachev@gmail.com>
2019-11-13 19:05:37 +0600

    up to date

--- src/components/presetInfo.ts
+++ src/components/presetInfo.ts
@@ -1,43 +1,63 @@
 export interface PresentInfo {
-  name: string;
-  description: string;
-  weight: string;
-  price: string;
-  images: string[];
+  name?: string;
+  description?: string;
+  weight?: string;
+  price?: string;
+  folder?: string;
+  images?: string[];
+  thumbs?: string[];
 }
 
 export const db: PresentInfo[] = [
   {
-    name: `Podarok 1`,
-    description: `Sladkii podarok`,
-    weight: `500g`,
-    price: `250t`,
+    name: `Кубик Золотой`,
+    description: ``,
+    weight: `200`,
+    price: `270`,
+    folder: "kubik_zolotoi",
     images: [
-      "//placekitten.com/1500/500",
-      "//placekitten.com/4000/3000",
-      "//placekitten.com/1500/1500",
-    ]
+      "back.jpg",
+      "front.jgp",
+      "pile.jpg",
+    ],
+    thumbs: [
+      "thumb_back.jpg",
+      "thumb_front.jgp",
+      "thumb_pile.jpg",
+    ],
   },
   {
-    name: `Podarok 2`,
-    description: `Sladkii podarok`,
-    weight: `500g`,
-    price: `250t`,
+    name: `Фонарик "Кто Так"`,
+    description: ``,
+    weight: `320`,
+    price: `490`,
+    folder: `kto_tam`,
     images: [
-      "//placekitten.com/1300/600",
-      "//placekitten.com/3000/2000",
-      "//placekitten.com/1400/1400",
-    ]
-  },{
-    name: `Podarok 3`,
-    description: `Sladkii podarok`,
-    weight: `500g`,
-    price: `250t`,
+      "back.jpg",
+      "front.jgp",
+      "pile.jpg",
+    ],
+    thumbs: [
+      "thumb_back.jpg",
+      "thumb_front.jgp",
+      "thumb_pile.jpg",
+    ],
+  },
+  {
+    name: `Лесной Домик`,
+    description: ``,
+    weight: `215`,
+    price: `300`,
     images: [
-      "//placekitten.com/1500/500",
+      "//placekitten.com/1500/1500",
       "//placekitten.com/4000/3000",
       "//placekitten.com/1500/1500",
-    ]
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
   },
   {
     name: `Podarok 4`,
@@ -48,17 +68,28 @@ export const db: PresentInfo[] = [
       "//placekitten.com/1300/600",
       "//placekitten.com/3000/2000",
       "//placekitten.com/1400/1400",
-    ]
-  },{
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
+  },
+  {
     name: `Podarok 5`,
     description: `Sladkii podarok`,
     weight: `500g`,
     price: `250t`,
     images: [
-      "//placekitten.com/1500/500",
+      "//placekitten.com/1500/1500",
       "//placekitten.com/4000/3000",
       "//placekitten.com/1500/1500",
-    ]
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
   },
   {
     name: `Podarok 6`,
@@ -69,17 +100,28 @@ export const db: PresentInfo[] = [
       "//placekitten.com/1300/600",
       "//placekitten.com/3000/2000",
       "//placekitten.com/1400/1400",
-    ]
-  },{
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
+  },
+  {
     name: `Podarok 7`,
     description: `Sladkii podarok`,
     weight: `500g`,
     price: `250t`,
     images: [
-      "//placekitten.com/1500/500",
+      "//placekitten.com/1500/1500",
       "//placekitten.com/4000/3000",
       "//placekitten.com/1500/1500",
-    ]
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
   },
   {
     name: `Podarok 8`,
@@ -90,6 +132,11 @@ export const db: PresentInfo[] = [
       "//placekitten.com/1300/600",
       "//placekitten.com/3000/2000",
       "//placekitten.com/1400/1400",
-    ]
+    ],
+    thumbs: [
+      "//placekitten.com/150/100",
+      "//placekitten.com/400/300",
+      "//placekitten.com/150/150",
+    ],
   },
 ];
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,51 +1,105 @@
 import React, { useState } from "react";
 import Lightbox from "react-image-lightbox";
+import styled from "styled-components";
+
 import Layout from "../components/layout";
-import { PresentInfo, db } from "../components/presetInfo";
+import { db, PresentInfo } from "../components/presetInfo";
+
+const PageHeader = styled.h1`
+  margin: 1rem;
+  font-size: 1.8rem;
+  font-family: "Playfair Display", serif;
+  text-align: center;
+`;
+
+const GalleryWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+`;
+
+const GalleryItem = styled.button`
+  display: flex;
+  flex-direction: column;
+  margin: 0.4rem;
+  padding: 0;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: transparent;
+  box-shadow: 5px 5px 5px rgba(169,50,47,0.35);
+  
+  img {
+    width: 100%;
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+  }
+  
+  span {
+    text-align: center;
+    margin-top: 0.5rem;
+    margin-bottom: 0.2rem;
+  }
+`;
 
 const GalleryPage: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [imageIndex, setImageIndex] = useState(0);
-  const [images, setImages] = useState(db[0].images);
+  const [images, setImages] = useState<string[]>([]);
+  const [thumbs, setThumbs] = useState<string[]>([]);
 
   const showGallery = (present: PresentInfo) => {
     setImages(present.images);
+    setThumbs(present.thumbs);
     setIsOpen(true);
   };
+  const closeGallery = () => {
+    setIsOpen(false);
+    setImages([]);
+    setThumbs([]);
+    setImageIndex(0);
+  };
 
-  const renderButtons = () => {
+  const renderGalleryItems = () => {
     return db.map((present: PresentInfo) => {
       return (
-        <button
+        <GalleryItem
           key={present.name}
           onClick={() => {
             showGallery(present);
           }}
         >
-          {present.name} - ({present.weight}, {present.price})
-        </button>
+          <img src={present.thumbs[0]} alt={present.name} />
+          <span>
+            {present.name} - ({present.weight}, {present.price})
+          </span>
+        </GalleryItem>
       );
     });
   };
 
   return (
     <Layout>
-      <h1>Лол, вот вам Галерея</h1>
-      {renderButtons()}
+      <PageHeader>Лол, вот вам Галерея</PageHeader>
+      <GalleryWrapper>{renderGalleryItems()}</GalleryWrapper>
       {isOpen && (
         <Lightbox
           mainSrc={images[imageIndex]}
           nextSrc={images[(imageIndex + 1) % images.length]}
           prevSrc={images[(imageIndex + images.length - 1) % images.length]}
-          onCloseRequest={() => {
-            setIsOpen(false);
-          }}
+          onCloseRequest={closeGallery}
           onMoveNextRequest={() => {
             setImageIndex((imageIndex + 1) % images.length);
           }}
           onMovePrevRequest={() => {
             setImageIndex((imageIndex + images.length - 1) % images.length);
           }}
+          mainSrcThumbnail={thumbs[imageIndex]}
+          nextSrcThumbnail={thumbs[(imageIndex + 1) % thumbs.length]}
+          prevSrcThumbnail={
+            thumbs[(imageIndex + thumbs.length - 1) % thumbs.length]
+          }
         />
       )}
     </Layout>
